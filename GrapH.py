c9242182f4af2d1647f715b18eea93d33c510d59
Tom <tomsom@github.com>
2019-11-30 01:41:42 -0700

    params and format testing

--- GrapH.py
+++ GrapH.py
@@ -15,40 +15,49 @@ def main() :
    global g 
 
    parser = argparse.ArgumentParser()
-   parser.add_argument("-s", "--start", help="Account to start graph at.", dest='start')
+   parser.add_argument("-s", "--start", help="Account to start graph at.", dest='user')
    parser.add_argument("-d", "--depth", help="Depth to recurse.", type=int, dest='depth')
+   parser.add_argument("--following", help="Include following.", action='store_true')
+   parser.add_argument("--followers", help="Include followers.", action='store_true')
+   parser.add_argument("--repos", help="Include repositories.", action='store_true')
    args = parser.parse_args()
 
-   g.add_node(args.start)
-   recurse(args.start, args.depth)
+   #g.add_node(args.user)
+   recurse(args.user, args.depth, args.following, args.followers, args.repos)
 
-   nx.draw_networkx(g, nodecolor='w', edge_color='b')
-   plt.show()
+   #nx.draw_spectral(g, with_labels=True, nodecolor='w', edge_color='b')
+   #nx.draw_planar(g, with_labels=True, nodecolor='w', edge_color='b')
+   #nx.draw_random(g, with_labels=True, nodecolor='w', edge_color='b')
+   #nx.draw_spring(g, with_labels=True, nodecolor='w', edge_color='b', font_size=12)
+   nx.draw_circular(g, font_size=12, with_labels=True, node_color='r')
 
-def recurse (cuser, cdepth) : 
- 
+   plt.axis('off')
+   plt.show() # display
+   print(g.degree)
+
+def recurse (cuser, cdepth, bfollowing, bfollowers, brepos) : 
+
    global g
-   following = userFollowing(cuser)
-   followers = userFollowers(cuser)
 
-   print(cuser, cdepth, following, followers)
-
-   for i in following : 
-      g.add_node(i)
-      g.add_edge(cuser, i)
-
-   for i in followers : 
-      g.add_node(i)
-      g.add_edge(i, cuser)
-
-   print(cdepth)
-   print(cdepth-1)
-
-   if cdepth > 0 : 
-      for i in following : 
-         recurse(i, cdepth-1)
+   if bfollowers : 
+      followers = userFollowers(cuser)
       for i in followers : 
-         recurse(i, cdepth-1) 
+         #g.add_node(i)
+         g.add_edge(i, cuser)
+
+   if bfollowing : 
+      following = userFollowing(cuser)
+      for i in following : 
+         #g.add_node(i)
+         g.add_edge(cuser, i)
+
+   #if cdepth > 0 : 
+   #   for i in followers : 
+   #      cargs.user = i
+   #      cargs.depth -= 1 
+   #      recurse(i, cdepth-1)
+   #   #for i in followers : 
+   #   #   recurse(i, cdepth-1) 
 
 if __name__ == "__main__" : 
    main()
